bddde7b61eefe3ca04143c0183ab69a42f3cbd26
AlirezaAshouri <152364693+AlirezaAshouri@users.noreply.github.com>
2023-11-29 09:50:48 +0330

    Add files via upload

---
+++ test8.py
@@ -0,0 +1,16 @@
+op = input("enter op(*,/+, -): ")
+a = int(input("enter a: "))
+b = int(input("enter b: "))
+
+if op == "+":
+    print(a+b)
+
+if op == "-":
+    print(a-b)
+
+if op == "*":
+        print(a*b)
+
+if op == "/" :
+     print(a/b)     
+
---
+++ test9.py
@@ -0,0 +1,10 @@
+a = int(input("enter a: "))
+b = int(input("enter b: "))
+c = int(input("enter c: "))
+
+if a+b>c and a+c>b and c+b>a:
+    print("triangle is created") 
+
+else:
+    print("triangle is not created")    
+    
